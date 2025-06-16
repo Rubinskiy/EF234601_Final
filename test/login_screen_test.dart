9c662ca7226193b1588ba096137c3fd63789b67d
Robin <roebebin@gmail.com>
2025-06-16 22:26:30 +0700

    fix: login widget test

--- test/login_screen_test.dart
+++ test/login_screen_test.dart
@@ -5,9 +5,10 @@ import 'package:final_project/main.dart';
 void main() {
   testWidgets('Login screen UI test', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-    await tester.pump(const Duration(seconds: 10));
+    await tester.pump(const Duration(seconds: 8));
     expect(find.text('CampusTracker'), findsOneWidget);
-    expect(find.byType(TextField), findsOneWidget);
+    expect(find.widgetWithText(TextField, 'Email'), findsOneWidget);
+    expect(find.widgetWithText(TextField, 'Password'), findsOneWidget);
     expect(find.widgetWithText(ElevatedButton, 'Login'), findsOneWidget);
   });
 }
